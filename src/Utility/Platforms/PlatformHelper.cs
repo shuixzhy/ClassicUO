1a7a3d6bcf44b2fccb32352ace6d19005828f413
数码熊 <shuixzy@qq.com>
2020-02-21 15:33:04 +0800

    删除文件 src

--- src/Utility/Platforms/PlatformHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace ClassicUO.Utility.Platforms
-{
-    internal static class PlatformHelper
-    {
-        public static readonly bool IsMonoRuntime = Type.GetType("Mono.Runtime") != null;
-    }
-}
